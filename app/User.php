82c1f323ba557011077b0ac4ab9917b2a97945b7
yotan616 <yotan616@gmail.com>
2017-12-17 06:37:11 +0000

    timeline

--- app/User.php
+++ app/User.php
@@ -99,6 +99,14 @@ class User extends Model implements AuthenticatableContract,
     public function is_following($userId) {
         return $this->followings()->where('follow_id', $userId)->exists();
     }
+    
+    //タイムラインにマイクロポストを取得するためのメソッド
+    public function feed_microposts()
+    {
+        $follow_user_ids = $this->followings()->lists('users.id')->toArray();
+        $follow_user_ids[] = $this->id;
+        return Micropost::whereIn('user_id', $follow_user_ids);
+    }
 }
 
 
